029cd7e4e3f8e1a388ab32d906f273c6df433daa
root <root@tendersfinder.co.uk>
2020-02-24 12:05:29 +0000

    update

--- src/eventsToConnections10.js
+++
@@ -1,6 +0,0 @@
-User name,Password,Access key ID,Secret access key,Console login link
-firdaws,rAnC!o6nSXMg,AKIASLEES5FB24DCPSOK,giApV3loHiIrsJP3m548LcMMSmBjTVZ/wphnygtZ,
-https://161339337027.signin.aws.amazon.com/console
-
-
-rAnC!o6nSXMg
